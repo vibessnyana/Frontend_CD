1af8555d7e2cc93beb5d23abda25e749539991a7
HafizhGhiyats29 <hafizhghiyats29@gmail.com>
2026-05-31 19:10:25 +0700

    change import assets navbar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/logo1.png";
+import logo from "../../assets/logo1.webp";
 
 export default function Navbar() {
   const navClass = ({ isActive }) =>
